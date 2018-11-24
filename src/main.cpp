a533ee80290b526fbb817fe81661bec19b3945f2
Asset34 <artemiy.kashintsev@gmail.com>
2018-11-25 02:26:17 +0300

    project: init

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <QApplication>
+
+#include "ui/mainwindow.hpp"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    MainWindow w;
+    w.resize(800, 600);
+    w.show();
+
+    return a.exec();
+}
