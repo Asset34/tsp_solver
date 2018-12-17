e30e3d79eb6c65246a7b9f4e9196e834dca6fba9
Asset34 <artemiy.kashintsev@gmail.com>
2018-12-18 01:08:02 +0300

    AntColonyControlWidget: set default values

--- src/ui/algorithm_control_widgets/antcolonycontrolwidget.cpp
+++ src/ui/algorithm_control_widgets/antcolonycontrolwidget.cpp
@@ -12,26 +12,26 @@ AntColonyControlWidget::AntColonyControlWidget(QWidget *parent)
     // Create iterations spin box
     m_iterationsSpinBox = new QSpinBox;
     m_iterationsSpinBox->setRange(1, 1e9);
-    m_iterationsSpinBox->setValue(1);
+    m_iterationsSpinBox->setValue(10);
 
     // Create ants spin box
     m_antsSpinBox = new QSpinBox;
     m_antsSpinBox->setRange(1, 1e9);
-    m_antsSpinBox->setValue(1);
+    m_antsSpinBox->setValue(5);
 
     // Create validators
     QDoubleValidator *validator = new QDoubleValidator(0.0, 1.0, 10, this);
 
     // Create evapolate factor
-    m_evapolateFactorLineEdit = new QLineEdit;
+    m_evapolateFactorLineEdit = new QLineEdit("0,1");
     m_evapolateFactorLineEdit->setValidator(validator);
 
     // Create alpha line edit
-    m_alphaLineEdit = new QLineEdit;
+    m_alphaLineEdit = new QLineEdit("1");
     m_alphaLineEdit->setValidator(validator);
 
     // Create beta line edit
-    m_betaLineEdit = new QLineEdit;
+    m_betaLineEdit = new QLineEdit("1");
     m_betaLineEdit->setValidator(validator);
 
     // Create layout
