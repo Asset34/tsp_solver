e808862316ebecd42dfd396669bbca329c35be40
Asset34 <artemiy.kashintsev@gmail.com>
2019-01-11 20:06:22 +0300

    TspAlgorithmWidget: redesign class

--- src/ui/tspalgorithmwidget.cpp
+++ src/ui/tspalgorithmwidget.cpp
@@ -2,21 +2,45 @@
 
 #include <QVBoxLayout>
 
+#include <QGroupBox>
+
+#include <QPushButton>
+
 #include <ui/boxes/tspalgorithmselectbox.hpp>
 #include <parameter_list_widget/parameterlistwidget.hpp>
+#include <ui/tspresultwidget.hpp>
 
 TspAlgorithmWidget::TspAlgorithmWidget(QWidget *parent)
-    : QWidget(parent)
+    : QWidget(parent),
+      m_lastResult{.status = false}
 {
     m_algorithmSelectBox = new TspAlgorithmSelectBox;
 
+    // Set default algorithm
+    m_currentAlgorithm = m_algorithmSelectBox->getAlgorithm();
+
+    // Create parameter list widget with appropriate QGroupBox and QLayout
     m_parameterListWidget = new ParameterListWidget;
-    m_parameterListWidget->updateParameters(*m_algorithmSelectBox->getAlgorithm());
+    m_parameterListWidget->updateWith(*m_currentAlgorithm);
+    QGroupBox *inputGroupBox = new QGroupBox("Input");
+    QVBoxLayout *inputLayout = new QVBoxLayout;
+    inputLayout->setContentsMargins(5, 0, 5, 0);
+    inputLayout->addWidget(m_parameterListWidget);
+    inputGroupBox->setLayout(inputLayout);
+
+    // Create result widget with appropriate QGroupBox and QLayout
+    m_resultWidget = new TspResultWidget;
+    QGroupBox *resultGroupBox = new QGroupBox("Result");
+    QVBoxLayout *resultLayout = new QVBoxLayout;
+    resultLayout->setContentsMargins(5, 0, 5, 0);
+    resultLayout->addWidget(m_resultWidget);
+    resultGroupBox->setLayout(resultLayout);
 
     m_layout = new QVBoxLayout;
     m_layout->setContentsMargins(0, 0, 0, 0);
     m_layout->addWidget(m_algorithmSelectBox);
-    m_layout->addWidget(m_parameterListWidget);
+    m_layout->addWidget(inputGroupBox);
+    m_layout->addWidget(resultGroupBox);
 
     setLayout(m_layout);
 
@@ -24,11 +48,27 @@ TspAlgorithmWidget::TspAlgorithmWidget(QWidget *parent)
         m_algorithmSelectBox,
         &TspAlgorithmSelectBox::algorithmChanged,
         this,
-        &TspAlgorithmWidget::updateParameters
+        &TspAlgorithmWidget::changeAlgorithm
         );
 }
 
-void TspAlgorithmWidget::updateParameters(TspAlgorithm *algorithm)
+const TspResult &TspAlgorithmWidget::getResult() const
 {
-    m_parameterListWidget->updateParameters(*algorithm);
+    return m_lastResult;
+}
+
+void TspAlgorithmWidget::changeAlgorithm(TspAlgorithm *algorithm)
+{
+    m_currentAlgorithm = algorithm;
+    m_parameterListWidget->updateWith(*algorithm);
+}
+
+void TspAlgorithmWidget::execute(const AdjacencyMatrix &matrix)
+{
+    TspResult result = m_currentAlgorithm->execute(matrix);
+
+    m_lastResult = result;
+    m_resultWidget->setResult(result);
+
+    emit executed();
 }
--- src/ui/tspalgorithmwidget.hpp
+++ src/ui/tspalgorithmwidget.hpp
@@ -3,11 +3,17 @@
 
 #include <QWidget>
 
+//#include <QVector>
+
 #include <algorithms/tspalgorithm.hpp>
 
+#include <adjacencymatrix.hpp>
+
 class QVBoxLayout;
+class QPushButton;
 class TspAlgorithmSelectBox;
 class ParameterListWidget;
+class TspResultWidget;
 
 class TspAlgorithmWidget : public QWidget
 {
@@ -16,17 +22,26 @@ class TspAlgorithmWidget : public QWidget
 public:
     explicit TspAlgorithmWidget(QWidget *parent = nullptr);
 
+    const TspResult &getResult() const;
+
+public slots:
+    void changeAlgorithm(TspAlgorithm *algorithm);
+    void execute(const AdjacencyMatrix &matrix);
+
 private:
     QVBoxLayout *m_layout;
 
     TspAlgorithmSelectBox *m_algorithmSelectBox;
     ParameterListWidget *m_parameterListWidget;
+    TspResultWidget *m_resultWidget;
 
-private slots:
-    void updateParameters(TspAlgorithm *algorithm);
+    TspAlgorithm *m_currentAlgorithm;
+    TspResult m_lastResult;
+
+signals:
+    void executed();
 
 };
-
 
 
 #endif // TSPALGORITHMWIDGET_HPP
