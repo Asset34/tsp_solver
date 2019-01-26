71cf845b1c246e0885deb9e4b51d6fabda34d4c2
Asset34 <artemiy.kashintsev@gmail.com>
2019-01-26 14:20:46 +0300

    Matrix: enable default constructor

--- src/matrix.hpp
+++ src/matrix.hpp
@@ -6,7 +6,7 @@
 class Matrix
 {
 public:
-    Matrix(int size, double defaultValue = 0.0);
+    Matrix(int size = 0, double defaultValue = 0.0);
 
     const std::vector<double> &operator[](int row) const;
     std::vector<double> &operator[](int row);
